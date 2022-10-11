9ea240a20e09bef3eef0f55a22a5f6a31287b930
Ba7arathinam <bala01225@gmail.com>
2022-10-11 19:32:50 +0530

    do some changes

--- module3(DOM)/dom.js
+++ module3(DOM)/dom.js
@@ -5,7 +5,7 @@ ele.innerHTML="<span class=main>this is span<span>"
 
 document.body.append(ele)
 
-// alter method
+// // alter method
 
 var div=document.createElement("div")
 div.setAttribute("class","main")
@@ -15,3 +15,15 @@ span.setAttribute("class","main1")
 
 div.append(span);
 document.body.append(div);
+
+//assignment sum
+var sign=document.createElement("div")
+var sign1=document.createElement("div")
+var sign2=document.createElement("div")
+sign.setAttribute("class","container")
+sign1.setAttribute("class","row")
+sign2.setAttribute("class","col")
+sign2.innerText="this is col"
+sign1.append(sign2)
+sign.append(sign1)
+document.body.append(sign)
